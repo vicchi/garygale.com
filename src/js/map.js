152fa711e339df62291ecad10b478cd66f7f5562
vicchi <gary@vicchi.org>
2014-05-02 12:24:09 +0100

    Remove map attribution from the map and attribute correctly in the page footer.

--- src/js/map.js
+++ src/js/map.js
@@ -9,37 +9,19 @@ var options = {
 	doubleClickZoom: false,
 	boxZoom: false,
 	keyboard: false,
-	zoomControl: false
+	zoomControl: false,
+	attributionControl: false
 };
 
-//console.log('default lat/lng: ' + options.center.toString());
 $.getJSON('/scripts/location.php')
 	.done(function(data) {
 		var coords = data.Placemark.Point.coordinates.split(',');
 		options.center = new L.LatLng(coords[1], coords[0]);
-		//console.log('current lat/lng: ' + options.center.toString());
 	})
 	.always(function(data) {
-		//console.log('using lat/lng: ' + options.center.toString());
-		layer = new L.StamenTileLayer('toner');
+		layer = new L.TileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png');
 		map = new L.Map('map', options);
 		map.addLayer(layer);
 		var bounds = map.getBounds();
 		map.setMaxBounds(bounds);
 	});
-
-/*var layer = new L.StamenTileLayer('toner');
-var map = new L.Map('map', {
-	center: new L.LatLng(51.4267, -0.3312),
-	zoom: 15,
-	dragging: false,
-	touchZoom: false,
-	scrollWheelZoom: false,
-	doubleClickZoom: false,
-	boxZoom: false,
-	keyboard: false,
-	zoomControl: false
-});
-map.addLayer(layer);
-var bounds = map.getBounds();
-map.setMaxBounds(bounds);*/
