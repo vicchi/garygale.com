aad0d97ac705fa32dd12f7b47492efdb8c13a21d
Gary Gale <gary@vicchi.org>
2023-05-01 10:57:57 +0100

    feat: package upgrades and rel=me additions

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,133 +1,156 @@
-sass = require('node-sass');
-module.exports = function(grunt) {
-    require('load-grunt-tasks')(grunt);
+sass = require("sass");
+module.exports = function (grunt) {
+    require("load-grunt-tasks")(grunt);
 
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+        pkg: grunt.file.readJSON("package.json"),
         clean: {
-            rebuild: ['public/']
+            rebuild: ["public/"],
         },
         concat: {
             preload: {
-                src: [
-                    'src/js/google-tracking.js'
-                ],
-                dest: 'public/assets/js/site.js'
-            }
+                src: ["src/js/google-tracking.js"],
+                dest: "public/assets/js/site.js",
+            },
         },
         copy: {
             html: {
-                files: [{
-                    expand: true,
-                    cwd: 'src/html',
-                    src: ['**/*.html'],
-                    dest: 'public/'
-                }]
+                files: [
+                    {
+                        expand: true,
+                        cwd: "src/html",
+                        src: ["**/*.html"],
+                        dest: "public/",
+                    },
+                ],
             },
             icons: {
-                files: [{
-                    expand: true,
-                    cwd: 'src/images',
-                    src: ['favicon.ico'],
-                    dest: 'public/'
-                }]
+                files: [
+                    {
+                        expand: true,
+                        cwd: "src/images",
+                        src: ["favicon.ico"],
+                        dest: "public/",
+                    },
+                ],
             },
             misc: {
-                files: [{
-                    expand: true,
-                    cwd: 'src/misc',
-                    src: ['**/*', '**/.*'],
-                    dest: 'public/'
-                }]
-            }
+                files: [
+                    {
+                        expand: true,
+                        cwd: "src/misc",
+                        src: ["**/*", "**/.*"],
+                        dest: "public/",
+                    },
+                ],
+            },
         },
         cssmin: {
             dist: {
-                files: [{
-                    expand: true,
-                    cwd: 'public/assets/css',
-                    src: ['*.css', '!*.min.css'],
-                    dest: 'public/assets/css',
-                    ext: '.min.css'
-                }]
-            }
+                files: [
+                    {
+                        expand: true,
+                        cwd: "public/assets/css",
+                        src: ["*.css", "!*.min.css"],
+                        dest: "public/assets/css",
+                        ext: ".min.css",
+                    },
+                ],
+            },
         },
         imagemin: {
             images: {
-                files: [{
-                    expand: true,
-                    cwd: 'src/',
-                    src: ['images/**/*.png', 'images/**/*.jpg'],
-                    dest: 'public/assets/'
-                }]
-            }
+                files: [
+                    {
+                        expand: true,
+                        cwd: "src/",
+                        src: ["images/**/*.png", "images/**/*.jpg"],
+                        dest: "public/assets/",
+                    },
+                ],
+            },
         },
         sass: {
             options: {
                 implementation: sass,
-                outputStyle: 'expanded',
-                indentType: 'tab',
-                indentWidth: 1
+                outputStyle: "expanded",
+                indentType: "tab",
+                indentWidth: 1,
             },
             dist: {
                 files: {
-                    'public/assets/css/site.css': 'src/sass/site.scss'
-                }
-            }
+                    "public/assets/css/site.css": "src/sass/site.scss",
+                },
+            },
         },
         uglify: {
             dist: {
                 files: {
-                    'public/assets/js/site.min.js': 'public/assets/js/site.js'
-                }
-            }
+                    "public/assets/js/site.min.js": "public/assets/js/site.js",
+                },
+            },
         },
         watch: {
             options: {
-                livereload: true
+                livereload: true,
             },
             grunt: {
-                files: ['Gruntfile.js'],
-                tasks: ['build']
+                files: ["Gruntfile.js"],
+                tasks: ["build"],
             },
             js: {
-                files: ['src/js/*.js'],
-                tasks: ['concat', 'uglify']
+                files: ["src/js/*.js"],
+                tasks: ["concat", "uglify"],
             },
             uglify: {
-                files: ['!public/assets/js/*.min.js'],
-                tasks: ['uglify']
+                files: ["!public/assets/js/*.min.js"],
+                tasks: ["uglify"],
             },
             sass: {
-                files: ['src/sass/*.scss'],
-                tasks: ['sass']
+                files: ["src/sass/*.scss"],
+                tasks: ["sass"],
             },
             cssmin: {
-                files: ['public/assets/css/*.css', '!public/assets/css/*.min.css'],
-                tasks: ['cssmin']
+                files: [
+                    "public/assets/css/*.css",
+                    "!public/assets/css/*.min.css",
+                ],
+                tasks: ["cssmin"],
             },
             html: {
-                files: ['src/html/**/*.html'],
-                tasks: ['copy:html']
+                files: ["src/html/**/*.html"],
+                tasks: ["copy:html"],
             },
             robots: {
-                files: ['src/misc/**'],
-                tasks: ['copy:robots']
-            }
-        }
+                files: ["src/misc/**"],
+                tasks: ["copy:robots"],
+            },
+        },
     });
 
-    grunt.registerTask('default', ['watch']);
+    grunt.registerTask("default", ["watch"]);
 
-    grunt.registerTask('nodsstore', function() {
-        grunt.file.expand({
-            filter: 'isFile',
-            cwd: '.'
-        }, ['**/.DS_Store']).forEach(function(file) {
-            grunt.file.delete(file);
-        });
+    grunt.registerTask("nodsstore", function () {
+        grunt.file
+            .expand(
+                {
+                    filter: "isFile",
+                    cwd: ".",
+                },
+                ["**/.DS_Store"]
+            )
+            .forEach(function (file) {
+                grunt.file.delete(file);
+            });
     });
 
-    grunt.registerTask('build', ['concat', 'copy', 'imagemin', 'sass', 'cssmin', 'uglify']);
-    grunt.registerTask('rebuild', ['clean', 'build']);
+    grunt.registerTask("build", [
+        "concat",
+        "copy",
+        "imagemin",
+        "sass",
+        "cssmin",
+        "uglify",
+    ]);
+    grunt.registerTask("rebuild", ["clean", "build"]);
 };
